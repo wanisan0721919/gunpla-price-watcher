4706176483363effbf7387bbf7b05bcf43a4a088
wanisan0721919 <723love723@gmail.com>
2025-04-10 14:40:52 +0900

    Add GitHub Actions workflow and Selenium script

--- gunpla_watcher.py
+++ gunpla_watcher.py
@@ -27,21 +27,23 @@ chromedriver_autoinstaller.install()
 # WebDriver作成
 driver = webdriver.Chrome(options=chrome_options)
 
-# 商品ページにアクセス
-driver.get("https://www.amazon.co.jp/RG-%E6%A9%9F%E5%8B%95%E6%88%A6%E5%A3%AB%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0UC-%E3%83%A6%E3%83%8B%E3%82%B3%E3%83%BC%E3%83%B3%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0-144%E3%82%B9%E3%82%B1%E3%83%BC%E3%83%AB-%E8%89%B2%E5%88%86%E3%81%91%E6%B8%88%E3%81%BF%E3%83%97%E3%83%A9%E3%83%A2%E3%83%87%E3%83%AB/dp/B08XWBS3WM/ref=sr_1_1?dib=eyJ2IjoiMSJ9.ydGC_Br1xhMandLowCJHyOTJDFXeRrf2UfWFUSFPF-6hQ6NeGOCrCpDMiP7A21Df_royV4Ace4QB-tV7Q5lMVh7kmDJJQ1g6H2vZyPwNiecSteZOGHPlt0rVZWyLu3o8pfSls-ecoLVA3O7VWYrerx3rAzNHNPLL-DaVr7qZvvp_ZC6Duk_KMQWd8lJNK_C1y4XVKfXJpunenThXhv6NzPScS0C7LMJWTCi6pMKS1ulSs4tCAyhyoSbrTb2dwmtRgVruE_u5NC3QHQyBKL-zS8GsbbS7bddke3C0L0lxklA.BVeOmAMFAzU9f5nEP8OjY-vhDtYA7XVMpmm6A-_6ODQ&dib_tag=se&m=AN1VRQENFRJN5&qid=1744251790&refinements=p_6%3AAN1VRQENFRJN5&s=hobby&sr=1-1")
+# Amazonガンプラページへアクセス
+driver.get("https://www.amazon.co.jp/RG-%E6%A9%9F%E5%8B%95%E6%88%A6%E5%A3%AB%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0UC-%E3%83%A6%E3%83%8B%E3%82%B3%E3%83%BC%E3%83%B3%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0-144%E3%82%B9%E3%82%B1%E3%83%BC%E3%83%AB-%E8%89%B2%E5%88%86%E3%81%91%E6%B8%88%E3%81%BF%E3%83%97%E3%83%A9%E3%83%A2%E3%83%87%E3%83%AB/dp/B08XWBS3WM")
 
-# 価格の要素が表示されるのを待つ
+# 商品がロードされるのを待機（最大30秒）
 WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]/span[2]'))
+    EC.presence_of_element_located((By.XPATH, '//*[@id="corePrice_feature_div"]//span[@class="a-price-whole"]'))
 )
 
-# 商品タイトルと価格を取得
-product_title = driver.find_element(By.XPATH, '//*[@id="productTitle"]/span')
-product_price = driver.find_element(By.XPATH, '//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[3]/span[2]/span[2]')
+# 商品名の取得
+product_title = driver.find_element(By.XPATH, '//*[@id="productTitle"]').text
 
-# 結果表示
-print(f"商品名: {product_title.text}")
-print(f"価格: ¥{product_price.text}")
+# 商品価格の取得
+product_price = driver.find_element(By.XPATH, '//*[@id="corePrice_feature_div"]//span[@class="a-price-whole"]').text
+
+# 結果を表示
+print(f"商品名: {product_title}")
+print(f"価格: {product_price}")
 
 # WebDriverを終了
 driver.quit()
