8879fc7bd3679c327b4343c2bb200c17e196eb66
wanisan0721919 <723love723@gmail.com>
2025-04-10 15:39:58 +0900

    Add GitHub Actions workflow and Selenium script

--- gunpla_watcher.py
+++ gunpla_watcher.py
@@ -31,10 +31,10 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.amazon.co.jp/RG-%E6%A9%9F%E5%8B%95%E6%88%A6%E5%A3%AB%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0UC-%E3%83%A6%E3%83%8B%E3%82%B3%E3%83%BC%E3%83%B3%E3%82%AC%E3%83%B3%E3%83%80%E3%83%A0-144%E3%82%B9%E3%82%B1%E3%83%BC%E3%83%AB-%E8%89%B2%E5%88%86%E3%81%91%E6%B8%88%E3%81%BF%E3%83%97%E3%83%A9%E3%83%A2%E3%83%87%E3%83%AB/dp/B08XWBS3WM")
 
 # 商品がロードされるのを待機（最大30秒）
-WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="productTitle"]').get_attribute("value")
-)
-)
+#WebDriverWait(driver, 30).until(
+#    EC.presence_of_element_located((By.XPATH, '//*[@id="productTitle"]').get_attribute("value")
+#)
+#)
 
 # 商品名の取得（get_attribute("value")を使用）
 product_title = driver.find_element(By.XPATH, '//*[@id="productTitle"]').get_attribute("value")
